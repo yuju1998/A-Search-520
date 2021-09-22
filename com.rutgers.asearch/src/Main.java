7abb445a74a17a00ece7caaca76440d25d6cca18
Jason Luo <luojason1227@gmail.com>
2021-09-22 15:54:56 -0400

    wrote tests for Questions 6 and 7

--- com.rutgers.asearch/src/Main.java
+++ com.rutgers.asearch/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -19,6 +20,42 @@ public class Main {
         String bumps = canSeeSideways ? "SideFov" : "NoSideFov";
         printResultsToCsv(xDimension + "x" + yDimension + bumps +"Result.csv", solutionDensity);
 
+    }
+
+    public static void runQuestion6(int xDim, int yDim, int numIter, int maxProb) {
+        String fileName = "Q6-AStar-Manhattan-NoBumps-Results.csv";
+        List<GridWorldInfo> results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::manhattanDistance), true, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-AStar-Euclidean-NoBumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::euclideanDistance), true, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-AStar-Chebyshev-NoBumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::chebyshevDistance), true, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-BFS-NoBumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, BFSObject::BFS, true, maxProb);
+        printResultsToCsv(fileName, results);
+    }
+
+    public static void runQuestion7(int xDim, int yDim, int numIter, int maxProb) {
+        String fileName = "Q6-AStar-Manhattan-Bumps-Results.csv";
+        List<GridWorldInfo> results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::manhattanDistance), false, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-AStar-Euclidean-Bumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::euclideanDistance), false, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-AStar-Chebyshev-Bumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, new AStarSearch(Heuristics::chebyshevDistance), false, maxProb);
+        printResultsToCsv(fileName, results);
+
+        fileName = "Q6-BFS-Bumps-Results.csv";
+        results = runThreeSearches(xDim, yDim, numIter, BFSObject::BFS, false, maxProb);
+        printResultsToCsv(fileName, results);
     }
 
     public static void printResultsToCsv(String fileName, List<GridWorldInfo> gridWorldInfo){
@@ -60,11 +97,52 @@ public class Main {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         int iterations = args.length > 2 ? Integer.parseInt(args[2]): 1000;
-        runProbabilitySimulation(x,y,iterations,false);
+        // runProbabilitySimulation(x,y,iterations,false);
+
+        runQuestion6(x, y, iterations, 33);
+        runQuestion7(x, y, iterations, 33);
     }
 
+    private static List<GridWorldInfo> runThreeSearches(int xDim, int yDim, int numIter, SearchAlgo algo, boolean canSeeSideways, int maxProb) {
+        // initialize variables needed for searching
+        Tuple<Integer, Integer> start = new Tuple<>(0, 0);
+        Tuple<Integer, Integer> end = new Tuple<>(xDim-1, yDim-1);
 
+        ArrayList<GridWorldInfo> results = new ArrayList<>((maxProb+1)*numIter);
+        for(int prob = 0; prob <= maxProb; ++prob) {
+            for(int iter = 0; iter < numIter; ++iter) { // test each probability numIter times
+                GridWorldInfo globalInfo = new GridWorldInfo(0, 0, new ArrayList<>()); // holds stats for this iteration
+                Grid grid;
+                while(true) { // keep generating gridworlds until one is solvable
+                    grid = new Grid(xDim, yDim, prob);
 
+                    // attempt search over complete gridworld and continue if it is solvable
+                    GridWorldInfo completeResult = algo.search(start, end, grid, cell -> cell.isBlocked());
+                    if(completeResult.getPath() != null) {
+                        globalInfo.setTrajectoryLengthComplete(completeResult.getTrajectoryLength());
+                        break;
+                    }
+                }
 
+                // run Repeated Search and record results
+                Robot robot = new Robot(start, end, canSeeSideways, grid, algo);
+                GridWorldInfo repeatedResult = robot.run();
+                globalInfo.setTrajectoryLength(repeatedResult.getTrajectoryLength());
+                globalInfo.setNumberOfCellsProcessed(repeatedResult.getNumberOfCellsProcessed());
+                globalInfo.setPath(repeatedResult.getPath());
+
+                // run search over discovered gridworld and record results
+                Predicate<GridCell> discoveredAndFree = robot.getKnownFreeSpaces()::contains;
+                Predicate<GridCell> undiscoveredOrBlocked = discoveredAndFree.negate();
+                GridWorldInfo discoveredResult = algo.search(start, end, grid, undiscoveredOrBlocked);
+                globalInfo.setTrajectoryLengthDiscovered(discoveredResult.getTrajectoryLength());
+
+                globalInfo.setProbability(prob);
+                results.add(globalInfo);
+            }
+        }
+
+        return results;
+    }
 
 }
