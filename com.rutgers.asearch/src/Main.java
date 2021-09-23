369327577c43fd346a574693066185d1e3dcd1bd
Jason Luo <luojason1227@gmail.com>
2021-09-22 20:17:35 -0400

    wrote tests for Question 9

--- com.rutgers.asearch/src/Main.java
+++ com.rutgers.asearch/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Main {
@@ -50,7 +51,18 @@ public class Main {
         runThreeSearches(fileName, xDim, yDim, numIter, BFSObject::BFS, false, maxProb);
     }
 
-    public static void runQuestion9(int xDim, int yDim, int numIter, int maxProb, double maxWeight) {
+    public static void runQuestion9(int xDim, int yDim, int numIter, int prob, double maxWeight, double step) {
+        String name1 = "Q9prob=" + prob + "-AStar-WeightedManhattan-NoBumps-Results.csv";
+        String name2 = "Q9prob=" + prob + "-RepeatedAStar-WeightedManhattan-NoBumps-Results.csv";
+        runWeightedSearch(name1, name2, xDim, yDim, numIter, Heuristics::manhattanDistance, prob, maxWeight, step);
+
+        name1 = "Q9prob=" + prob + "-AStar-WeightedEuclidean-NoBumps-Results.csv";
+        name2 = "Q9prob=" + prob + "-RepeatedAStar-WeightedEuclidean-NoBumps-Results.csv";
+        runWeightedSearch(name1, name2, xDim, yDim, numIter, Heuristics::euclideanDistance, prob, maxWeight, step);
+
+        name1 = "Q9prob=" + prob + "-AStar-WeightedChebyshev-NoBumps-Results.csv";
+        name2 = "Q9prob=" + prob + "-RepeatedAStar-WeightedChebyshev-NoBumps-Results.csv";
+        runWeightedSearch(name1, name2, xDim, yDim, numIter, Heuristics::chebyshevDistance, prob, maxWeight, step);
 
     }
 
@@ -95,8 +107,9 @@ public class Main {
         int iterations = args.length > 2 ? Integer.parseInt(args[2]): 1000;
         // runProbabilitySimulation(x,y,iterations,false);
 
-        runQuestion6(x, y, iterations, 33);
-        runQuestion7(x, y, iterations, 33);
+        // runQuestion6(x, y, iterations, 33);
+        // runQuestion7(x, y, iterations, 33);
+        runQuestion9(x, y, iterations, 15, 10.0, 0.2);
     }
 
     private static void runThreeSearches(String fileName, int xDim, int yDim, int numIter, SearchAlgo algo, boolean canSeeSideways, int maxProb) {
@@ -139,6 +152,56 @@ public class Main {
         }
 
         printResultsToCsv(fileName, results);
+    }
+
+    private static void runWeightedSearch(
+        String fileName1, String fileName2,
+        int xDim, int yDim, int numIter,
+        BiFunction<Tuple<Integer, Integer>, Tuple<Integer, Integer>, Double> heuristic,
+        int prob, double maxWeight, double step
+    ) {
+        // initialize variables needed for searching
+        Tuple<Integer, Integer> start = new Tuple<>(0, 0);
+        Tuple<Integer, Integer> end = new Tuple<>(xDim-1, yDim-1);
+        SearchAlgo aStar = new AStarSearch(Heuristics::manhattanDistance); // used to test for solvability
+
+        ArrayList<GridWorldInfo> results = new ArrayList<>(); // for regular A*
+        ArrayList<GridWorldInfo> repeatedResults = new ArrayList<>(); // for repeated A*
+        for(int iter = 0; iter < numIter; ++iter) {
+            // keep generating grids until a solvable one is found
+            Grid grid = new Grid(xDim, yDim, prob);
+            while(aStar.search(start, end, grid, cell -> cell.isBlocked()).getPath() == null) {
+                grid = new Grid(xDim, yDim, prob);
+            }
+
+            for(int i = 0; 1.0 + i*step <= maxWeight; ++i) {
+                // create search object using weighted heuristic
+                double weight = 1.0 + i*step;
+                SearchAlgo weightedAStar = new AStarSearch((coord1, coord2) -> weight*heuristic.apply(coord1, coord2));
+
+                // run regular A* with the weighted heuristic and record the result
+                long startTime = System.nanoTime();
+                GridWorldInfo result = weightedAStar.search(start, end, grid, cell -> cell.isBlocked());
+                long endTime = System.nanoTime();
+                result.setProbability(prob);
+                result.setWeight(weight);
+                result.setRuntime(endTime - startTime);
+                results.add(result);
+
+                // run repeated A* with the weight heuristic snd record the result
+                Robot robot = new Robot(start, end, true, grid, weightedAStar);
+                startTime = System.nanoTime();
+                GridWorldInfo repeatedResult = robot.run();
+                endTime = System.nanoTime();
+                repeatedResult.setProbability(prob);
+                repeatedResult.setWeight(weight);
+                repeatedResult.setRuntime(endTime - startTime);
+                repeatedResults.add(repeatedResult);
+            }
+        }
+
+        printResultsToCsv(fileName1, results);
+        printResultsToCsv(fileName2, repeatedResults);
     }
 
 }
